c9c5ee74024b0a565b1ff7a30b7b63cad7efe95a
Sarang Tamrakar <sarang.tamrakar@flutura.com>
2023-07-29 16:59:29 +0530

    slidw-show started

---
+++ components/SlideshowComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SlideshowComponent = () => {
+  return (
+    <div>
+      <h2>Slide Show</h2>
+    </div>
+  );
+}
+
+export default SlideshowComponent;
---
+++ pages/slide-show.js
@@ -0,0 +1,12 @@
+import SlideshowComponent from '@/components/SlideshowComponent';
+import React from 'react';
+
+const Slideshow = () => {
+  return (
+    <div>
+          <SlideshowComponent></SlideshowComponent>
+    </div>
+  );
+}
+
+export default Slideshow;
